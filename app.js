6ccfba949caa041adb5795c01607e22061139375
Kris Lafrance <Kris.lafrance6067@gmail.com>
2019-06-05 15:06:09 -0700

    finished basic functionality

--- app.js
+++ app.js
@@ -1,39 +1,54 @@
 //listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  //Hide results
+  document.getElementById('results').style.display = 'none';
+  //Show Loader
+  document.getElementById('loading').style.display = 'block';
 
-//Calculate Results
-function calculateResults(e) {
-  console.log('Calc')
-// UI Variables
-const amount = document.getElementById('amount');
-const interest = document.getElementById('interest');
-const years = document.getElementById('years');
-const monthlyPayment = document.getElementById('monthly-payment');
-const totalPayment = document.getElementById('total-payment');
-const totalInterest = document.getElementById('total-interest');
-
-const principal = parseFloat(amount.value);
-const calculatedInterest = parseFloat(interest.value) / 100 / 12;
-const calculatedPayments = parseFloat(years.value) * 12;
-
-//Compute monthly payment
-const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-const monthly = (principal * x * calculatedInterest)/(x-1);
-
-if(isFinite(monthly)) {
-  monthlyPayment.value = monthly.toFixed(2);
-  totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-  totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
-} else {
-  showError('Please Check your numbers');
-}
+  setTimeout(calculateResults, 2000);
 
   e.preventDefault();
+});
+
+//Calculate Results
+function calculateResults() {
+  console.log('Calc')
+  // UI Variables
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  //Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest)/(x-1);
+
+  if(isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+    //Show results
+    document.getElementById('results').style.display = 'block';
+    //Hide Loader
+    document.getElementById('loading').style.display = 'none';
+  } else {
+    showError('Please Check your numbers');
+  }
 }
 
 //USE JQuery to make a fadout feature?
 
 function showError(error) {
+  //hide results
+  document.getElementById('results').style.display = 'none';
+  //Hide Loader
+  document.getElementById('loading').style.display = 'none';
   //Create div
   const errorDiv = document.createElement('div');
   //Get elements
